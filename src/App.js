10a360bf7f3e59eb8303a5cb3c885da1cf916839
Victor Galindo <victorgalindo475@gmail.com>
2024-06-22 21:54:00 -0700

    Updated project structure and files for responsive grid

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende React Ahora
+          Aprende React Today Sunday
         </a>
       </header>
     </div>
