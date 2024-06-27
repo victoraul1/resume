1f10f93f9e3aa20722c524134525a929210fecf6
Victor Galindo <victorgalindo475@gmail.com>
2024-06-27 00:18:53 -0700

    Updated project structure and files for responsive grid

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -29,17 +29,28 @@ function Resume() {
           <section>
             <h3>Skills</h3> 
             <div className="underline"></div>
-            <p><strong>DESIGN SOFTWARES:</strong> Adobe Photoshop, Adobe Premierer</p>
-            <p><strong>Programming:</strong> HTML, CSS, Javascript React, Python, Flutter</p>
+            <p><strong>Programming:</strong> HTML, CSS, Javascript, Node.js, nginx, React, Python, Flutter</p>
             <p><strong>Analytics:</strong> Google Analytics, Adobe Analytics</p>
             <p><strong>AI: </strong>chatGPT, NLP, Machine Learning, Generative AI</p>
+            <p><strong>Design Software:</strong> Adobe Photoshop, Adobe Premiere</p>
           </section>
 
           <section>
-            <h3>Awards</h3>
+            <h3>Projects | Portfolio</h3>
             <div className="underline"></div>
-            <p><strong>LOGO DESIGN AWARD</strong> Contest Lorem Ipsum February 2014</p>
-            <p><strong>DESIGN AWARD</strong> Contest Lorem Ipsum February 2014</p>
+            
+            <p><strong>React</strong> www.readmysubjectline.com</p>
+            <p><strong>PHP</strong> www.moveyourcar.org</p>
+            <p><strong>Flutter</strong> www.constitucion.pe</p>
+            <p><strong>Wordpress</strong><br /> www.mimenu.pe<br /> www.automovil.tech<br /> www.elecciones2026.pe<br /> www.hn-rewards.com</p>
+            
+          </section>
+
+          <section>
+            <h3>Certifications</h3>
+            <div className="underline"></div>
+            <p><strong>Salesforce</strong> Certified Marketing Cloud Email Specialist</p>
+            
           </section>
         </div>
 
@@ -49,41 +60,111 @@ function Resume() {
             <div className="underline"></div>
             <div className="job">
               <div className="job-header">
-                <h4>ENTER JOB POSITION HERE</h4>
-                <span>2014 - Present</span>
+                <h4>Digital Marketing Specialist</h4>
+                <span>2022 - Present</span>
               </div>
-              <p><em>Company / Location</em></p>
+              <p>Herbalife | Torrance, CA</p>
+             <br />
               <ul>
-                <li>Managed lorem ipsum dolor sit amet dolor ipsum lorem.</li>
-                <li>Class aptent taciti sociosqu ad litora torquent per conubia nostra.</li>
-                <li>Worked in variety lorem ipsum dolor ipsum.</li>
-                <li>Responsible for lorem ipsum dolor sit amet lorem ipsum dolor.</li>
+                Email Studio:
+                <li>Build, test, deploy, and metrics report</li>
+                <li>Email journey building</li>
+                <li>Audience segmentation and automations</li>
+              </ul>
+
+              <ul>
+                SMS:
+                <li>Setup and schedule sends</li>
+                <li>Audience list management</li>
+                <li>Process documentation and training</li>
+              </ul>
+
+              <ul>
+                Adobe Experience Manager:
+                <li>Build and update pages</li>
+                <li>Setup e-commerce product pages</li>
+                <li>Asset management</li>
+              </ul>
+
+              <ul>
+              Strategy:
+                <li>Provide recommendatons based on metrics</li>
+                <li>Build wireframes for better engagement</li>
+                <li>Advice on Email subject lines and pre-header</li>
+              </ul>
+
+            </div>
+            <div className="job">
+              <div className="job-header">
+                <h4>Salesforce Email Studio Administrator</h4>
+                <span>2018 - 2020</span>
+              </div>
+              <p>Fox 20th Century Studios | Century City, CA</p>
+             
+              <ul>
+                Email Studio:
+                <li>Build, test, deploy, provide reports</li>
+                <li>Email journey building</li>
+                <li>SFMC user administration</li>
+                <li>OKTA admin</li>
+                <li>Audience segmentation</li>
+              </ul>
+
+              <ul>
+              Web CMS
+                <li>Build and update pages</li>
+                <li>Google Analytics admin</li>
               </ul>
             </div>
             <div className="job">
               <div className="job-header">
-                <h4>ENTER JOB POSITION HERE</h4>
-                <span>2009 - 2014</span>
+                <h4>Web Operations Developer</h4>
+                <span>2017 - 2018</span>
               </div>
-              <p><em>Company / Location</em></p>
+              <p>American Botanical Pharmacy | Marina del Rey, CA</p>
               <ul>
-                <li>Managed lorem ipsum dolor sit amet dolor ipsum lorem.</li>
-                <li>Class aptent taciti sociosqu ad litora torquent per conubia nostra.</li>
-                <li>Worked in variety lorem ipsum dolor ipsum.</li>
-                <li>Responsible for lorem ipsum dolor sit amet lorem ipsum dolor.</li>
+              Mailchimp Administrator:
+              <li>Build, test, deploy, and metrics report</li>
+              <li>Email automations</li>
+              <li>Audience segmentation</li>
+              </ul>
+
+              <ul>
+              CMS Management:
+              <li>Build and update pages</li>
+              <li>Setup e-commerce product pages</li>
+              <li>Asset management</li>
+              <li>Google Analytics metrics</li>
+              <li>Social media PPC campaign management</li>
               </ul>
             </div>
+
             <div className="job">
               <div className="job-header">
-                <h4>ENTER JOB POSITION HERE</h4>
-                <span>2007 - 2009</span>
+                <h4>Web Content Publisher</h4>
+                <span>2013 - 2017</span>
               </div>
-              <p><em>Company / Location</em></p>
+              <p>Herbalife | Torrance, CA</p>
               <ul>
-                <li>Managed lorem ipsum dolor sit amet dolor ipsum lorem.</li>
-                <li>Class aptent taciti sociosqu ad litora torquent per conubia nostra.</li>
-                <li>Worked in variety lorem ipsum dolor ipsum.</li>
-                <li>Responsible for lorem ipsum dolor sit amet lorem ipsum dolor.</li>
+              Salesforce ExactTarget:
+              <li>Build, test, deploy, and metrics report</li>
+              <li>Email journey building</li>
+              <li>Audience segmentation and automations</li>
+              <li>Process documentation and training</li>
+              </ul>
+
+              <ul>
+              Teamsite (CMS):
+              <li>Build and update pages</li>
+              <li>Setup e-commerce product pages</li>
+              <li>Asset management</li>
+              </ul>
+
+              <ul>
+                SMS:
+                <li>Setup and schedule sends</li>
+                <li>Audience list management</li>
+                <li>Process documentation and training</li>
               </ul>
             </div>
           </section>
@@ -92,12 +173,12 @@ function Resume() {
             <h3>Education</h3>
             <div className="underline"></div>
             <div className="education-item">
-              <p><strong>MASTERS DEGREE in graphic design</strong><span>2011 - 2015</span></p>
-              <p>New York University</p>
+              <p><strong>BACHELORS DEGREE in Information Technology</strong><span> 2014 - 2017</span></p>
+              <p>University of Phoenix</p>
             </div>
             <div className="education-item">
-              <p><strong>BACHELORS DEGREE in graphic design</strong><span>2007 - 2011</span></p>
-              <p>New York College</p>
+              <p><strong>ASSOCIATES DEGREE in Computer Networking</strong><span> 2004 - 2007</span></p>
+              <p>College of the Canyons</p>
             </div>
           </section>
         </div>
